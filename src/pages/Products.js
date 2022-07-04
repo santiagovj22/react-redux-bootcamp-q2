2df2c7e8b6b5f3a211bc15167f7a0846caf64d83
santiago.valle <santiago.valle@COGL6WR3TW7Y.local>
2022-07-04 04:50:17 -0500

    added oroducts page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,37 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { ProductCard } from '../components/products/ProductCard'
+import { Container } from '../styles/pages/Products.styles';
 export const Products = () => {
+
+  const [data, setData] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const result = await (await fetch('data/products.json')).json();
+      setData(result.data?.products?.items || []);
+    } catch (error) {
+      throw error;
+    }
+  };
+  const handleCart = (product) => {
+    alert(' Nothing to do here yet');
+  }
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div>Products</div>
+    <>
+    <Container>
+     {
+        data.length > 0 && data.map((product) => {
+          return <ProductCard 
+                    key={product.id} 
+                    product={product} 
+                    handleCart={handleCart}/>   
+        })
+     }
+    </Container>
+    </>
   )
 }
